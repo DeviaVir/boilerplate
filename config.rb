0a0daa6638f37268bd7b2cc4c0e98fa74478eb57
Chase Sillevis <chase@sillevis.net>
2012-02-16 14:52:41 +0100

    Init

---
+++ config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+preferred_syntax = :sass
